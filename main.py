ccd91c0c926bfb3668920815cd2c39a08497c2d0
Markus Koch <markuskoch.social@icloud.com>
2025-12-03 12:10:42 +0100

    feat: create rest api

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
+import pickle
+import pandas as pd
 from fastapi import FastAPI
 
-from schema import InputData
+from ml.data import process_data
+from ml.model import inference
+from schema import PredictPayload
+from train_model import cat_features
+
+
+with open('model/model.pkl', 'rb') as model_file:
+    config = pickle.load(model_file, encoding='utf-8')
 
 app = FastAPI()
 
@@ -9,5 +18,11 @@ async def greeting():
     return { 'message': 'Hello, FastAPI!' }
 
 @app.post('/predict')
-async def predict(data: InputData):
-    return { 'message': str(data.foo) }
+async def predict(payload: PredictPayload):
+    df = pd.DataFrame([payload.model_dump()])
+    df.rename(lambda key: key.replace('_', '-'), axis='columns', inplace=True)
+
+    x, _, _, _ = process_data(df, cat_features, None, False, config['encoder'], config ['lb'])
+    preds = inference(config['model'], x)
+
+    return { 'salary': '>50K' if preds[0] else '<=50K' }
--- schema.py
+++ schema.py
@@ -1,5 +1,17 @@
 from pydantic import BaseModel, Field
 
-class InputData(BaseModel):
-    foo: str = Field()
-
+class PredictPayload(BaseModel):
+    age: int = Field(examples=[18])
+    workclass: str = Field(examples=['Private'])
+    fnlgt: int = Field(examples=[309634])
+    education: str = Field(examples=['11th'])
+    education_num: int = Field(examples=[7])
+    marital_status: str = Field(examples=['Never-married'])
+    occupation: str = Field(examples=['Other-service'])
+    relationship: str = Field(examples=['Own-child'])
+    race: str = Field(examples=['White'])
+    sex: str = Field(examples=['Female'])
+    capital_gain: int = Field(examples=[0])
+    capital_loss: int = Field(examples=[0])
+    hours_per_week: int = Field(examples=[22])
+    native_country: str = Field(examples=['United-States'])
